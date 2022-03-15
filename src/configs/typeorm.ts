3881ebc6aae126bdf9e8a01fb7a9d2f142a9eddf
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-03-15 23:31:33 +0400

    updated some prod stuff

--- src/configs/typeorm.ts
+++ src/configs/typeorm.ts
@@ -4,58 +4,58 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export class TypeormConfig {
-    private static _instance: TypeOrmModuleOptions;
+  private static _instance: TypeOrmModuleOptions;
 
-    private constructor(private env: { [k: string]: string | undefined }) {}
+  private constructor(private env: { [k: string]: string | undefined }) {}
 
-    public static get instance(): TypeOrmModuleOptions {
-        if (!this._instance) {
-            this._instance = new this(process.env)
-                .ensureValues([
-                    'POSTGRES_HOST',
-                    'POSTGRES_PORT',
-                    'POSTGRES_USERNAME',
-                    'POSTGRES_PASSWORD',
-                    'POSTGRES_DATABASE',
-                ])
-                .getTypeOrmConfig();
-        }
-
-        return this._instance;
+  public static get instance(): TypeOrmModuleOptions {
+    if (!this._instance) {
+      this._instance = new this(process.env)
+        // .ensureValues([
+        //     'POSTGRES_HOST',
+        //     'POSTGRES_PORT',
+        //     'POSTGRES_USERNAME',
+        //     'POSTGRES_PASSWORD',
+        //     'POSTGRES_DATABASE',
+        // ])
+        .getTypeOrmConfig();
     }
 
-    public ensureValues(keys: string[]) {
-        keys.forEach((key) => {
-            const value = this.env[key];
+    return this._instance;
+  }
 
-            if (!value) {
-                throw new Error(`config error - missing env.${key}`);
-            }
-        });
+  public ensureValues(keys: string[]) {
+    keys.forEach((key) => {
+      const value = this.env[key];
 
-        return this;
-    }
+      if (!value) {
+        throw new Error(`config error - missing env.${key}`);
+      }
+    });
 
-    public getTypeOrmConfig(): TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: process.env.POSTGRES_HOST,
-            port: parseInt(process.env.POSTGRES_PORT),
-            username: process.env.POSTGRES_USERNAME,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DATABASE,
-            logging: false,
-            synchronize: false,
-            migrationsTableName: 'migrations',
-            migrations: ['dist/src/database/migrations/*.js'],
-            entities: [__dirname + '/../**/*.entity.{js,ts}'],
-            useUTC: true,
+    return this;
+  }
 
-            cli: {
-                migrationsDir: 'src/database/migration',
-            },
-        };
-    }
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      logging: false,
+      synchronize: false,
+      migrationsTableName: 'migrations',
+      migrations: ['dist/src/database/migrations/*.js'],
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      useUTC: true,
+
+      cli: {
+        migrationsDir: 'src/database/migration',
+      },
+    };
+  }
 }
 
 // old configs
--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,34 @@ import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { AllExceptionsFilter } from "./app/commons/filters/generic_exception.filter";
+import { AllExceptionsFilter } from './app/commons/filters/generic_exception.filter';
 import { AppModule } from './app/modules/app/app.module';
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe } from '@nestjs/common';
 
 NestFactory.create<NestExpressApplication>(AppModule).then(async (app) => {
-    app.set('trust proxy', 1);
-    app.useGlobalPipes(new ValidationPipe({transform: true}));
-    app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
-    app.enableCors({origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', credentials: true,});
-    app.use(session({secret: 'my-secret',  resave: false, saveUninitialized: false,}),);
-    app.use(cookieParser());
-    app.use(helmet());
-    app.use(compression());
-    await app.listen(process.env.PORT);
+  app.set('trust proxy', 1);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
+  app.use(session({ secret: 'my-secret', resave: false, saveUninitialized: false }));
+  app.use(cookieParser());
+  app.use(helmet());
+  app.use(compression());
+  app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
-    console.log({
-        platform: 'nestjs',
-        library: 'express',
-        enviroment: process.env.NODE_ENV,
-        port: process.env.PORT
-    });
+  await app.listen(process.env.PORT);
+
+  console.log({
+    platform: 'nestjs',
+    library: 'express',
+    enviroment: process.env.NODE_ENV,
+    port: process.env.PORT,
+  });
 });
-
 
 /**
  * //!!!!!! https://swiperjs.com/demos for awesome swiping experience
