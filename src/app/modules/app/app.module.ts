bbf8278ca9d0de73c87f9c2cc905502f2d28db0a
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-03-15 23:47:45 +0400

    disabled typeorm for now

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -1,26 +1,26 @@
 import * as path from 'path';
 import { Module } from '@nestjs/common';
-import { ThrottlerModule } from "@nestjs/throttler";
-import { JwtModule } from "@nestjs/jwt";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { TypeormConfig } from "../../../configs/typeorm";
-import { MulterModule } from "@nestjs/platform-express";
-import { AppController } from "./app.controller";
-import { MovieModule } from "../movies/movie.module";
+import { ThrottlerModule } from '@nestjs/throttler';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeormConfig } from '../../../configs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { AppController } from './app.controller';
+import { MovieModule } from '../movies/movie.module';
 
 @Module({
-    imports: [
-        ThrottlerModule.forRoot({ttl: 60, limit: 100}), // 100 request every minute
-        JwtModule.register({}),
-        ConfigModule.forRoot({ isGlobal: true }),
-        TypeOrmModule.forRoot(TypeormConfig.instance),
-        MulterModule.register({ dest: path.join(__dirname, '../../../../', 'upload') }),
+  imports: [
+    ThrottlerModule.forRoot({ ttl: 60, limit: 100 }), // 100 request every minute
+    JwtModule.register({}),
+    ConfigModule.forRoot({ isGlobal: true }),
+    // TypeOrmModule.forRoot(TypeormConfig.instance),
+    MulterModule.register({ dest: path.join(__dirname, '../../../../', 'upload') }),
 
-        // own modules
-        MovieModule
-    ],
-    controllers: [AppController],
-    providers: [],
+    // own modules
+    MovieModule,
+  ],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
